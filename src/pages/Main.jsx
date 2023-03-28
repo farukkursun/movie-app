b3b18c8b4227c4b01f1baf0024ae43695f5f7370
faruk <kursunomerfaruk2@gmail.com>
2023-03-28 22:16:54 +0200

    update loading

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -8,43 +8,39 @@ const ApiKey = "2ffb1cad850221d084465c45e6fd0612";
 const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${ApiKey}`;
 const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${ApiKey}&query=`;
 
-
-
-
 const Main = () => {
-  const {currentUser}= useContext(MovieContext);
+  const { currentUser } = useContext(MovieContext);
   const [movies, setmovies] = useState([]);
   const [searchText, setSearchText] = useState("");
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     getMovies(FEATURED_API);
   }, []);
 
   const getMovies = (api) => {
+    setLoading(true);
     axios
       .get(api)
       .then((res) => setmovies(res.data.results))
-      .catch((err) => console.log(err));
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
   };
 
-
-const handleSubmit =(e)=>{
-  e.preventDefault()
-if(searchText && currentUser){
-  getMovies(SEARCH_API + searchText)
-}
-else if (!currentUser){
-  toastWarning('please log in to')
-}else{
-  toastWarning('enter a text')
-}
-setSearchText('')
-
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (searchText && currentUser) {
+      getMovies(SEARCH_API + searchText);
+    } else if (!currentUser) {
+      toastWarning("please log in to");
+    } else {
+      toastWarning("enter a text");
+    }
+    setSearchText("");
+  };
 
   return (
     <>
-      <form className="search" onSubmit={handleSubmit} >
+      <form className="search" onSubmit={handleSubmit}>
         <input
           type="search"
           className="searchinput"
@@ -57,9 +53,13 @@ setSearchText('')
         </button>
       </form>
       <div className="main">
-        {movies.map((movie) => (
-          <Moviecard key={movie.id} {...movie} />
-        ))}
+        {loading ? (
+          <div class="spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+          </div>
+        ) : (
+          movies.map((movie) => <Moviecard key={movie.id} {...movie} />)
+        )}
       </div>
     </>
   );
